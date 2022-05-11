7a1d1ec6deb1ac1aff4f73bcba5094f216b08412
LandenTy <93301002+LandenTy@users.noreply.github.com>
2022-05-10 20:37:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,14 +8,13 @@ import Matrix_Projection_001 as mm
 import Engine as e
 from math import sin, cos
 
-# Variables
-angle = 10
-
-# Matrixes and Vector List(s)
+# Matrix(s) and Vector List(s)
 projection = [[1, 0, 0], [0, 1, 0]]
-rotateX = [[cos(angle), -sin(angle)], [-sin(angle), cos(angle)]]
-rotateY = [[cos(angle), -sin(angle)], [sin(angle), cos(angle)]]
 vectorList = []
+
+# Angle Degree Variables
+angleZ = 0
+angleY = 0
 
 # Points
 p1 = e.Vector(vectorList, [[-50], [-50], [0]])
@@ -23,16 +22,25 @@ p2 = e.Vector(vectorList, [[50], [-50], [0]])
 p3 = e.Vector(vectorList, [[-50], [50], [0]])
 p4 = e.Vector(vectorList, [[50], [50], [0]])
 
-p5 = e.Vector(vectorList, [[-50], [-50], [-10]])
-p6 = e.Vector(vectorList, [[50], [-50], [-10]])
-p7 = e.Vector(vectorList, [[-50], [50], [-10]])
-p8 = e.Vector(vectorList, [[50], [50], [-10]])
-
 # Program
-for x in range(len(vectorList)):
+while True:
     
-    projected = mm.matmul(projection, vectorList[x])
-    rotated = mm.matmul(rotateX, projected)
-    e.Vertice((255, 255, 255), rotated)
+    # Rotation Matrixes
+    rotateZ = [[cos(angleZ), -sin(angleZ)], [-sin(angleZ), cos(angleZ)]]
+    
+    #rotateY = [[cos(angleY), -sin(angleY)], [sin(angleY), cos(angleY)]]
+    
+    # Clear Screen
+    e.clearScreen()
+    
+    # Draws Vertices
+    for x in range(len(vectorList)):
+    
+        projected = mm.matmul(projection, vectorList[x])
+        rotatedZ = mm.matmul(rotateZ, projected)
+        e.Vertice((255, 255, 255), rotatedZ)
+        
+    # Adds to Angles
+    angleZ += 0.01
 
 e.Window(True)
